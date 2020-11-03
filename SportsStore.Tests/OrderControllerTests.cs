6cd36a8ad24cd1567ddca0b6d1d8e132310ea715
Zhao Tian <Zhao.Tian@igiltd.com>
2020-11-03 16:31:05 +0800

    finish cart and order feature

---
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        void Cannot_checkout_empty_cart()
+        {
+            //Arrange 
+            var mockRepo = new Mock<IOrderRepository>();
+            var cart = new Cart();
+            var order = new Order();
+
+            var sut = new OrderController(mockRepo.Object, cart);
+            
+            //Act
+            var result = sut.Checkout(order) as ViewResult;
+
+            //Assert
+            mockRepo.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        void Cannot_checkout_invalid_shipping_details()
+        {
+            //Arrange 
+            var mockRepo = new Mock<IOrderRepository>();
+            var cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            var order = new Order();
+
+            var sut = new OrderController(mockRepo.Object, cart);
+            sut.ModelState.AddModelError("error", "error");
+            //Act
+            var result = sut.Checkout(order) as ViewResult;
+
+            //Assert
+            mockRepo.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        void Can_checkout_and_submit_order()
+        {
+            //Arrange 
+            var mockRepo = new Mock<IOrderRepository>();
+            var cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            var order = new Order();
+
+            var sut = new OrderController(mockRepo.Object, cart);
+
+            //Act
+            var result = sut.Checkout(order) as RedirectToPageResult;
+
+            //Assert
+            mockRepo.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+            Assert.Equal("/Completed", result.PageName);
+        }
+    }
+}
--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -6,9 +6,37 @@ namespace SportsStore.Controllers
 
     public class OrderController : Controller
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly Cart _cartService;
+
+        public OrderController(IOrderRepository orderRepository, Cart cartService)
+        {
+            _orderRepository = orderRepository;
+            _cartService = cartService;
+        }
+
         public ViewResult Checkout()
         {
             return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cartService.Lines.Count == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cartService.Lines.ToArray();
+                _orderRepository.SaveOrder(order);
+                _cartService.Clear();
+                return RedirectToPage("/Completed", new {orderId = order.OrderId});
+            }
+
+            return View();
         }
     }
 }
