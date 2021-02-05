efea306c0022bdb19fc34f1319a17c10f8310f3c
Zhao Tian <Zhao.Tian@igiltd.com>
2021-02-05 14:43:06 +0000

    add admin login/out page

---
+++ SportsStore/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginModel {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if ((await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid name or password");
+            return View(loginModel);
+        }
+
+        [Authorize]
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin"; 
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            var context = app
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<AppIdentityDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userMananger = app
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userMananger.FindByIdAsync(adminUser);
+
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                user.Email = "admin@example.com";
+                user.PhoneNumber = "12345678";
+                await userMananger.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
